53b527f2c48be939c86b3a05e9939b47062ce82c
Satyam <satyammitthu@gmail.com>
2023-06-09 22:19:23 +0530

    Day_3 start

--- Day 3/pattern8.cpp
+++ Day 3/pattern8.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int main()
 {
+	int n;
+	cin>>n;
+
+	int i=1;
+	int count=1;
+	while (i<=n){
+		int j=1;
+	
+		while(j<=i){
+		 cout<<count<<" ";
+		 j=j+1;
+		 count=count+1;
+		}
+	 cout<<endl;
+	 i=i+1;
+	
+	}
  return 0;
 }
