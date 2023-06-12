3462d94b03dc67c0c6ee2b7f2cb333ade0ee85e8
Satyam <satyammitthu@gmail.com>
2023-06-12 22:10:30 +0530

    loop

---
+++ Day 5/forloop.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*int n;
+	cout<<"Enter the value of n"<<endl;
+	cin>>n;
+	cout<<"print count from 1 to n"<<endl;
+	for (int i = 1; i <=n; i++)
+	{
+		cout<<i<<endl;
+	}*/
+	
+ 
+
+
+//whithout mention in for bracket
+
+int n ;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    cout<<"print count from 1 to n"<<endl;
+        int i =1;
+for(; ; ){
+	if(i<=n){
+	  cout<< i <<endl;
+	} 
+	else{
+		break;
+	}
+	i++;
+}
+    
+
+
+ return 0;
+}
