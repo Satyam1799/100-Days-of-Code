6a4a4ec8ddd2a337abd75a0b10f687852d0c1926
Satyam <satyammitthu@gmail.com>
2023-06-13 11:00:30 +0530

    continue_start

---
+++ Day 6/continue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=0 ; i<5 ; i++){
+		cout<<" Hii "<<endl;
+		cout<<" Hey "<<endl;
+
+        continue;
+		cout<<" Kuch to bol"<<endl;
+		
+	}
+ return 0;
+}
