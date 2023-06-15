5911138ebdf02de3f7289e7895153e4d0c6ff459
Satyam <satyammitthu@gmail.com>
2023-06-15 22:20:14 +0530

    calculator

---
+++ Day 8/calculator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the value of a"<<endl;
+	cin>>a;
+
+	int b;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+
+	char op;
+	cout<<"Enter the operation that you want to perform"<<endl;
+	cin>>op;
+
+	switch (op)
+	{
+	case '+' : cout<< (a+b)<<endl;
+		break;
+	case '-' : cout<< (a-b)<<endl;
+	    break;
+	case '*' : cout<< (a*b)<<endl;
+	    break;
+	case '/' : cout<< (a/b)<<endl;
+	    break;
+	case '%' : cout<< (a%b)<<endl;
+	    break;
+
+	
+
+	
+	default:cout <<"Please enter a valid operation"<<endl;
+		break;
+	}
+
+ return 0;
+}
