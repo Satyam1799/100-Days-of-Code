9fab2d8a8c058052ee33bede67dcd4b18767532d
Satyam <satyammitthu@gmail.com>
2023-06-17 23:20:51 +0530

    sum

---
+++ Day 10/sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5]={2 , 7 , 1 , -4 , 11};
+
+	int sum=0;
+
+	for(int i = 0 ; i< 5 ; i++){
+		sum = sum + arr[i];
+	}
+	cout<<"sum of array is "<< sum <<endl;
+ return 0;
+}
