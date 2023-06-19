5f9682a72f47de83410ef5ea32d4ed8286ccbffb
Satyam <satyammitthu@gmail.com>
2023-06-19 22:20:01 +0530

    swapalternate

--- Day 10/reversealter.cpp
+++ Day 10/reversealter.cpp
@@ -1,33 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void Alternate(int arr[], int n){
-	int start=0;
-	int next =start+1;
-
-	for (int i = 0; i < n; i= i+2)
-	{
-		swap(arr[i], arr[i+1]);
-	}
-	
-}
-
 void printArray(int arr[] , int n){
-	for(int i = 0 ; i<n ; i++){
-		cout<<arr[i]<<" ";
 
+	for( int i =0 ; i<n ;i++){
+		cout<<arr[i]<<" ";
+		
 	}
-cout<<endl;
+	cout<<endl;
 }
 
+void swapAlternate (int arr[] , int size){
+	for( int i =0 ; i<size ; i+=2){
+		if( i+1< size){
+			swap(arr[i] , arr[i+1]);
+		}
+	}
 
+
+}
 int main()
 {
 
-	int arr[6] = {1,2,3,4,5,6};
+	int even[6] ={1,2,3,4,5,6};
+	int odd[5] ={11,22,33,44,55};
 
-	Alternate(arr ,6);
+	swapAlternate(even , 6);
+	printArray(even , 6);
 
-	printArray(arr,6);
+	swapAlternate(odd , 5);
+	printArray(odd, 5);
+
+
  return 0;
 }
